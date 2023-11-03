16fecc05e281c5aa2d1c94a8b344c106d479dd1a
Hyejung Hwang <seathat33@gmail.com>
2023-11-03 19:36:45 +0900

    feat: flask request 사용하는 것으로 변경

--- api/user.py
+++ api/user.py
@@ -2,8 +2,11 @@ from flask import Blueprint, request
 
 user_route = Blueprint('user_route', __name__)
 
-@user_route.route("/signup")
+@user_route.route("/signup",methods=["POST"])
 def signup():
-    print("helooo", flush=True)
-    return "Hello World"
+    email_receive = request.form['email_give']
+    name_receive = request.form['name_give']
+    print("helooo",flush=True)
+    print(f"received: {email_receive},{name_receive}",flush=True)
+    # return "Hello World"
 

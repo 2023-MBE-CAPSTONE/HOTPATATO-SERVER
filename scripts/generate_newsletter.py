c04e1b7e4040f83eb0b9ddda7f8538114ab9581c
Hyejung Hwang <seathat33@gmail.com>
2023-12-05 19:40:03 +0900

    feat: LangChain 사용

--- scripts/generate_newsletter.py
+++ scripts/generate_newsletter.py
@@ -1,24 +1,35 @@
 from datetime import datetime
 import sys
 import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 
 from dotenv import load_dotenv
 from dynamodb.issue_keywords import IssueKeywords
+from langchain.llms import OpenAI
 
-from models.issue_keyword import IssueKeyword
-from utils.date import get_today_date
+from chain import build_query_chain
 
-
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 if __name__ == "__main__":
-    # get issue keyword
     ik = IssueKeywords()
 
-    response = ik.query_by_issue_date(20231205)
-    print(response)
-    
-    # generate news letter
-    # TODO
+    response = ik.query_by_issue_date("20231205")
+    issue_keyword_data = response[0]
 
-    
+    llm = OpenAI(
+        temperature=0.5,              
+        max_tokens=2000,             
+        model_name='gpt-4',  
+    )
+    chain, response_format, output_parser = build_query_chain(llm)
+    response = chain.run(
+        {
+            'issue_name':issue_keyword_data["issue_name"],
+            'positive_keyword':issue_keyword_data["positive_keyword"],
+            'negative_keyword':issue_keyword_data["negative_keyword"],
+            "response_format": response_format
+        }
+    )
+    query = output_parser.parse(response)["positive_article"]
+    print(query)
